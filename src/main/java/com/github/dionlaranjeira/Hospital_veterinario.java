c625c63141e1e8151eb96a0ae566acb47c437857
DION RIBEIRO LARANJEIRA <dionribeiro.rr@gmail.com>
2021-10-18 21:39:30 -0400

    Initial commit

---
+++ src/main/java/com/github/dionlaranjeira/Hospital_veterinario.java
@@ -0,0 +1,13 @@
+package com.github.dionlaranjeira;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hospital_veterinario {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hospital_veterinario.class, args);
+    }
+}
